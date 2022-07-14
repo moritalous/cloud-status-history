5001239b55da6a204a184bf7e93c9b803c99f576
moritalous <moritalous@gmail.com>
2022-07-14 12:03:31 +0000

    AWSも広告更新

--- app/src/components/AWS/index.tsx
+++ app/src/components/AWS/index.tsx
@@ -2,20 +2,33 @@
  import Stack from 'aws-northstar/layouts/Stack';
  import StatusTable from './components/StatusTable';
 
+ const bannerUrl = [
+    'https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=42&l=ur1&category=echo_show_15&banner=0XH7MYVZZ3ASGXAQYJG2&f=ifr&linkID=d8c96a6976f4b73d15b4ccaa0fcf5a67&t=twelve02-22&tracking_id=twelve02-22',
+    'https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=42&l=ur1&category=smp&banner=1KWNB2B5BZKA90YG4RG2&f=ifr&linkID=8a635c66071275cfcd0b5f9224afbd7b&t=twelve02-22&tracking_id=twelve02-22',
+    'https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=42&l=ur1&category=echo_buds&banner=1T7F36BJ0887G8797M82&f=ifr&linkID=37a993e3fc5b9dc7f730e1f1d74f4916&t=twelve02-22&tracking_id=twelve02-22',
+    // 'https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=42&l=ur1&category=amazon_ring&banner=1G0EDMAKEN6X9TXGEW82&f=ifr&linkID=8d0f182da97f644d6393faab7fdd3556&t=twelve02-22&tracking_id=twelve02-22'
+    ]
+
+    const bannerList:Array<ReactElement> = []
+    bannerUrl.forEach(url => {
+        bannerList.push(<iframe src={url}
+        width={234}
+        height={60}
+        scrolling={'no'}
+        frameBorder={'0'}
+        marginWidth={0}
+        style={{border:'none', marginRight: '16px'}}
+        sandbox={'allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation'}
+    ></iframe>)
+    })
+
+
   const AWS: FunctionComponent = () => {
       return (
           <Stack>
               <StatusTable />
               <div style={{textAlign: 'center'}}>
-              <iframe src={'https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=48&l=ur1&category=jpprimeday&banner=0Y03CNA07BKTRXKWCNG2&f=ifr&linkID=b1d06824092a17cf6a6da8d9b24aa562&t=twelve02-22&tracking_id=twelve02-22'}
-                    width={728}
-                    height={90}
-                    scrolling={'no'}
-                    frameBorder={'0'}
-                    marginWidth={0}
-                    style={{border:'none'}}
-                    sandbox={'allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation'}
-            ></iframe>
+                {bannerList}
               </div>
           </Stack>
       );
